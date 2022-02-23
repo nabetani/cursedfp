473b4a5c30bc3784eee716c8c5b34ea391dd583f
Nabetani <takenori@nabetani.sakura.ne.jp>
2022-02-23 11:46:09 +0900

    動くようにする

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+typedef long double num_t;
+// typedef double num_t;
+
+num_t fabsll(num_t x)
+{
+    return x < 0 ? -x : x;
+}
+
+long double test(int n)
+{
+    num_t sum = 0;
+    num_t c = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        num_t v = (1.0L / i)*(1.0L / i);
+        num_t t = sum + v;
+        sum = t;
+    }
+    return sum + c;
+}
+
+int main(int argc, char const *argv[])
+{
+    struct timespec start={0}, end={0};
+    clock_gettime(CLOCK_REALTIME, &start);
+    long double r = test(10000000);
+    clock_gettime(CLOCK_REALTIME, &end);
+    time_t ds = end.tv_sec - start.tv_sec;
+    time_t dn = end.tv_nsec - start.tv_nsec;
+    printf("sizeof(num_t) = %zu\n", sizeof(num_t));
+    printf("r=%.20Lf t=%f\n", r, ds + dn * 1e-9);
+    return 0;
+}
